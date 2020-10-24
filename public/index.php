a36526fe1f32e1b82b6b61daa8df50837fe18143
Moisés Mariano <moisesduartem@gmail.com>
2020-10-24 17:20:23 -0300

    routes example

--- public/index.php
+++ public/index.php
@@ -4,3 +4,16 @@
  * Require the COMPOSER PSR-4 AUTOLOAD
  */
 require __DIR__ . '/../vendor/autoload.php';
+
+$env = parse_ini_file(__DIR__ . '/../.env');
+foreach ($env as $key => $value) {
+    define($key, $value);
+}
+
+$router = new \Bootstrap\Router(BASE_URL);
+
+$router->get('test/{name}', [ 'className' => 'Test', 'method' => 'test1' ]);
+
+$router->get('test2', [ 'className' => 'Test', 'method' => 'test2' ]);
+
+$router->run();
